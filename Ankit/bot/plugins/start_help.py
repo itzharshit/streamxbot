cac2d339c557668c575ade2c764458151200dbe5
Harshit Shrivastav <87630299+itzharshit@users.noreply.github.com>
2022-11-05 12:21:48 +0530

    Update start_help.py

--- Ankit/bot/plugins/start_help.py
+++ Ankit/bot/plugins/start_help.py
@@ -1,15 +1,15 @@
 # (c) adarsh-goel 
-from Adarsh.bot import StreamBot
-from Adarsh.vars import Var
+from Ankit.bot import StreamBot
+from Ankit.vars import Var
 import logging
 logger = logging.getLogger(__name__)
-from Adarsh.bot.plugins.stream import MY_PASS
-from Adarsh.utils.human_readable import humanbytes
-from Adarsh.utils.database import Database
+from Ankit.bot.plugins.stream import MY_PASS
+from Ankit.utils.human_readable import humanbytes
+from Ankit.utils.database import Database
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
-from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
+from Ankit.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
 
